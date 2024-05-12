b1c9ebfe1fcf93c168448e7ff90ef6888cfcb776
[Antonio Curione] <antonytheplayer@hotmail.it>
2024-05-12 18:28:04 +0200

    Aggiunto costruttore network

--- myNet.py
+++ myNet.py
@@ -1,4 +1,30 @@
-class myNet:
-    def __init__(self):
-        self.name = 'net'
-        
+import numpy as np
+import myActFunct as act
+
+def newNetwork(input_size, hidden_size, output_size):
+    sigma = 0.1
+    biases = []
+    weights = []
+    act_funct = []
+    prev_layer = input_size
+    
+    if np.isscalar(hidden_size):
+        hidden_size = [hidden_size]
+
+    for layer in hidden_size:
+        biases.append(sigma * np.random.normal(size = [layer, 1]))
+        weights.append(sigma * np.random.normal(size = [layer, prev_layer]))
+        prev_layer = layer
+    act_funct = setActFunct()
+    biases.append(sigma * np.random.normal(size = [output_size, 1]))
+    weights.append(sigma * np.random.normal(size = [output_size, prev_layer]))
+    act_funct.append(act.identity)
+    net = []
+    
+    return net
+
+def setActFunct():
+    pass
+    
+
+    
